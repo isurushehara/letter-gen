cb648022604c99a8c593115dacaa08ef59c78057
Isuru Shehara <isurushehara@gmail.com>
2026-02-24 18:40:19 +0530

    feat: update PDF generation to use new headless mode and additional launch arguments

--- server/controllers/letterController.js
+++ server/controllers/letterController.js
@@ -139,8 +139,16 @@ exports.generatePDF = async (req, res) => {
     }
 
     const browser = await puppeteer.launch({
-      headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      headless: "new",
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-accelerated-2d-canvas",
+        "--no-first-run",
+        "--no-zygote",
+        "--single-process",
+      ],
     });
 
     const page = await browser.newPage();
